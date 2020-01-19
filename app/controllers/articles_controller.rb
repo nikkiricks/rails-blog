b859640b0a43b4db32147674fc39ac17dbdb0e0e
nikkiricks <nikki.ricks@gmail.com>
2020-01-19 18:17:15 +1100

    adding podcast API for searching and adding new podcasts from Listen Notes API

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
+require 'unirest'
+require '.env'
+
+@response = Unirest.get "https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1390190241000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=1",
+  headers:{
+    "X-ListenAPI-Key",
+  }
+
 
 class ArticlesController < ApplicationController
 
-   
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
  
   def index
