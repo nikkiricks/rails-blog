cebcf321b11d1f2b7aa614a91dbca093fb84e162
nikkiricks <nikki.ricks@gmail.com>
2020-01-20 19:45:11 +1100

    added users table and bcrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_100647) do
+ActiveRecord::Schema.define(version: 2020_01_20_084208) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_01_16_100647) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "email"
+    t.text "password_digest"
+    t.text "first_name"
+    t.text "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "articles"
